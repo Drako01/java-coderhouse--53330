db8aca6ff5ea2a04c97a2cbe458baae410278163
Alejandro Di Stefano <addistefano@live.com.ar>
2024-03-06 22:03:08 -0300

    Clase 05

--- final-proyect/ventas-online/src/main/java/com/coder/house/ventas/online/ventasonline/repository/ComprobanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coder.house.ventas.online.ventasonline.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coder.house.ventas.online.ventasonline.entidad.Comprobante;
-
-public interface ComprobanteRepository extends JpaRepository<Comprobante, Integer> {
-
-}
--- final-proyect/ventas-online/src/main/java/com/coder/house/ventas/online/ventasonline/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coder.house.ventas.online.ventasonline.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coder.house.ventas.online.ventasonline.entidad.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-
-}
