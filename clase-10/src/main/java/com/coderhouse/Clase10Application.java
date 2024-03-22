af15c9139c00b35b1b3483da6ae4b387d4d772bd
Alejandro Di Stefano <addistefano@live.com.ar>
2024-03-21 21:04:53 -0300

    CoderHouse

---
+++ clase-10/src/main/java/com/coderhouse/Clase10Application.java
@@ -0,0 +1,13 @@
+package com.coderhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase10Application.class, args);
+	}
+
+}
