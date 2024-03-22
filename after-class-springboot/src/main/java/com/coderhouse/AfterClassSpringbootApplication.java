f5757ebb23e4a6ef2945909514d27a8b6a0b636b
Alejandro Di Stefano <addistefano@live.com.ar>
2024-03-22 18:49:28 -0300

    CoderHouse

---
+++ after-class-springboot/src/main/java/com/coderhouse/AfterClassSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterClassSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterClassSpringbootApplication.class, args);
+	}
+
+}
